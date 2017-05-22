8d27175570fe51b1e781d42505e6dde041dec031
Graham Anderson <grahama@me.com>
2017-05-22 17:41:48 -0500

    simple express server setup

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
+// var express = require('express');
+//
+// // Create our app
+// var app = express();
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect('http://' + req.hostname + req.url);
+//     // res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+//   } else {
+//     next();
+//   }
+// });
+//
+// app.use(express.static('public'));
+//
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port ' + PORT);
+// });
+
+
 var express = require('express');
-
-// Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    // res.redirect('http://' + req.hostname + req.url);
-    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
-  } else {
-    next();
-  }
-});
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
-});
+app.listen(process.env.PORT || 8080);
