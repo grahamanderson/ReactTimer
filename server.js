aba03453fa11a51e6a7fbd86014e684c218db271
Graham Anderson <grahama@me.com>
2017-05-22 20:12:29 -0500

    rquired path

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-// var express = require('express');
-//
-// // Create our app
-// var app = express();
-// const PORT = process.env.PORT || 3000;
-//
-// app.use(function (req, res, next){
-//   if (req.headers['x-forwarded-proto'] === 'https') {
-//     res.redirect('http://' + req.hostname + req.url);
-//     // res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
-//   } else {
-//     next();
-//   }
-// });
-//
-// app.use(express.static('public'));
-//
-// app.listen(PORT, function () {
-//   console.log('Express server is up on port ' + PORT);
-// });
-
-
 var express = require('express');
+var path = require('path');
+
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    // res.redirect('http://' + req.hostname + req.url);
+    res.sendFile(path.resolve(__dirname + '/../dist/index.html'));
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
+});
+
+
+// var express = require('express');
+// var app = express();
+//
+// app.use(express.static('public'));
+//
+// app.listen(process.env.PORT || 8080);
